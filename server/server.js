e0efbeff4ce986546c7ef8b49dac1939d6786ac6
IanAdamPrice <ianadamprice@gmail.com>
2022-05-02 14:54:18 -0400

    heroku fix

--- server/server.js
+++ server/server.js
@@ -3,15 +3,18 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const app = express();
+const PORT =  process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${ PORT }`);
+});
 
 const contactEmail = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
+  port: 3001,
   secure: true,
   auth: {
     type: "OAuth2",
